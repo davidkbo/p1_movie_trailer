251dc768ff4ebb841a6cc620c69acfd85ef7c72b
David Kato <davidkbo@gmail.com>
2015-02-17 16:32:19 -0200

    Organizing files

--- ecenter.py
+++ ecenter.py
@@ -1,36 +1,36 @@
-import media
+import moviedb
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story" , 
+toy_story = moviedb.Movie("Toy Story" , 
   "Story of a boy and his toys that come to life", 
   "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
   "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar" , "A marine on an alien planet" , 
+avatar = moviedb.Movie("Avatar" , "A marine on an alien planet" , 
   "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
   "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-scarface = media.Movie("Scarface" , 
+scarface = moviedb.Movie("Scarface" , 
   "A determined Cuban immigrant takes over a drug cartel while succumbing to greed." , 
   "http://upload.wikimedia.org/wikipedia/pt/9/9c/Scarfaceposter.jpg",
   "https://www.youtube.com/watch?v=a9LVwNum6rE")
 
-school_of_rock = media.Movie("School of Rock" , 
+school_of_rock = moviedb.Movie("School of Rock" , 
   "Using rock music to learn" , 
   "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
   "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
-ratatouille = media.Movie("Ratatouille" , 
+ratatouille = moviedb.Movie("Ratatouille" , 
   "A rat is a chef in Paris" , 
   "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
   "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
-midnight_in_paris = media.Movie("Midnight in Paris" , 
+midnight_in_paris = moviedb.Movie("Midnight in Paris" , 
   "Going back in time to meet authors" , 
   "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
   "https://www.youtube.com/watch?v=BYRWfS2s2v4")
 
-hunger_games = media.Movie("Hunger Games" , 
+hunger_games = moviedb.Movie("Hunger Games" , 
   "A really real reality show" , 
   "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
   "https://www.youtube.com/watch?v=PbA63a7H0bo")
--- moviedb.py
+++ moviedb.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 
-# Import the imdb package.
-import imdb
+# # Import the imdb package.
+# import imdb
 
-# Create the object that will be used to access the IMDb's database.
-ia = imdb.IMDb()
+# # Create the object that will be used to access the IMDb's database.
+# ia = imdb.IMDb()
 
-# Search for a movie (get a list of Movie objects).
-s_result = ia.search_movie('The Untouchables')
+# # Search for a movie (get a list of Movie objects).
+# s_result = ia.search_movie('The Untouchables')
 
-# Print the long imdb canonical title and movieID of the results.
-for item in s_result:
-   print item
+# # Print the long imdb canonical title and movieID of the results.
+# for item in s_result:
+#    print item
 
-# Retrieves default information for the first result (a Movie object).
+# # Retrieves default information for the first result (a Movie object).
 
 
-
-class Movie():
-  def __init__(imdb_number, trailer_youtube):
+class Movie():  
+  #Initializing the variables for the class
+  def __init__(self, title, story_line, poster_image, trailer_youtube):
     self.title = title
     self.story_line = story_line
-    #self.duration = duration
     self.poster_image_url = poster_image
     self.trailer_youtube_url = trailer_youtube
-    ia = imdb.IMDb()
-
-
-
